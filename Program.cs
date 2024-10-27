ac67065f4882ef01fb344dab52667a5574ac1a59
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-10-28 00:02:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Traženje unosa od korisnika
+            Console.WriteLine("Unesite niz znakova:");
+            string unos = Console.ReadLine();
+
+            // Zamjena razmaka znakom '_'
+            string rezultat = unos.Replace(' ', '_');
+
+            // Ispis rezultata
+            Console.WriteLine("Niz nakon zamjene razmaka: " + rezultat);
+        }
+    }
+}
